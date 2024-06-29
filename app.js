2a7f1f85ca626f9e7494dd21bd2edc28dafcec99
Anirudh Kumar <anirudhkumar98188@gmail.com>
2024-06-29 15:52:45 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// var x = document.getElementById("one");
+// var x1 = document.getElementById("two");
+// var x2 = document.getElementById("three");
+// var x3 = document.getElementById("four");
+// var y = 0
+// setInterval(function(){
+   
+//      if(y<50){
+//         y++
+//         x.innerHTML = y;
+//      } 
+//      else 
+//      {
+//      clearInterval
+//      }
+
+// },10)
+// var y1 = 0;
+// setInterval(function(){
+   
+//      if(y1<550){
+//         y1++
+//         x1.innerHTML = y1;
+//      } 
+//      else 
+//      {
+//      clearInterval
+//      }
+
+// },-100)
+
+
+
+// console.log(x);
